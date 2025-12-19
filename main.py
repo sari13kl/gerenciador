f9406bc7d56bd2a21ff4bdfc9397bb81f9572ca2
Sarah Lopes <sasarinha13@gmail.com>
2025-12-19 19:01:22 -0300

    arquitetura do projeto

---
+++ main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+from auth_routes import auth_router
+from order_routes import order_router
+
+app.include_router(auth_router)
+app.include_router(order_router)
+
+#para rodar o servidor, use o comando:
+# uvicorn main:app --reload
+
+#endpoint:
+#/ordens(caminho)
+
+# Rest APIs
+# Get -> leitura/pegar
+# Post -> enviar/criar
+# Put/Patch -> atualizar/editar
+# Delete -> deletar/remover
+
