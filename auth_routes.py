b76843f6a4082847b22669cbe71ed7811c793aad
Sarah Lopes <sasarinha13@gmail.com>
2025-12-23 19:13:29 -0300

    implementação autenticação, strutered

--- auth_routes.py
+++ auth_routes.py
@@ -1,10 +1,25 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from models import User
+from dependencies import pegar_sessao
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
 @auth_router.post("")
-async def autenticar():
+async def home():
     """"
     Rota padrão de pedidos . Todas precisam ser autenticadas
     """
     return {"mensagem": "Rota de autenticação!", "autenticado": False}
+
+@auth_router.post("/cadastre")
+async def register_user(email: str, password: str, name: str, session = Depends(pegar_sessao())):
+    user = session.query(User).filter(User.email==email).first()
+    if user:
+        #já existe um usuário com esse email
+        return {"mensagem": "Já existe um usuário cadastrado com esse email!"}
+    else:
+        new_user = User(name, email, password)
+        session.add(new_user)
+        session.commit()
+        return {"mensagem": "Usuário cadastrado com sucesso!"}
+    
