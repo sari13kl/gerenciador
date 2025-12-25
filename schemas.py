efedd58e4d2f50bf74181a4a79185af39a36ffee
LuizHenriquerg <luizshenriquegomess@outlook.com>
2025-12-25 19:28:32 -0300

    feat: Introduce Pydantic schemas, refactor user registration, add order creation endpoint, and include an Alembic migration.

---
+++ schemas.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class UserSchema(BaseModel):
+    name: str
+    email: str
+    password: str
+    ativo: Optional[bool]
+    admin: Optional[bool]
+
+    class Config:
+        from_attributes = True
+
+
+class PedidoSchema(BaseModel):
+    user_id: int
+
+    class Config:
+        from_attributes = True
